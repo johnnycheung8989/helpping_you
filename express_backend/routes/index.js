874289c5e06a6e93182cb032e4abb234b03dbfe5
ironman <ironman@local.local>
2024-07-05 02:06:43 +0800

    hello

--- express_backend/routes/index.js
+++ express_backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'mac好好用' });
+  res.render('index', { title: 'windows 2002 好好用' });
 });
 
 module.exports = router;
