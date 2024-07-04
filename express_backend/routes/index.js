7337b04127036cc82f482ff3772af3f76bc9a7f1
HowardPoon <133858105+HowardPoon@users.noreply.github.com>
2024-07-05 01:15:25 +0800

    KAN-25

---
+++ express_backend/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'mac好好用' });
+});
+
+module.exports = router;
